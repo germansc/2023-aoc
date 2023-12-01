48b20b286ba95bbec8545d710f907062570492a7
germansc <germanscillone@gmail.com>
2023-12-01 17:37:36 -0300

    Initial solution for Day 1.

---
+++ day1/src/main.rs
@@ -0,0 +1,62 @@
+use std::io;
+
+fn main() {
+    println!("2023 AoC - Day 1");
+
+    let mut sum: i32 = 0;
+    let strings: Vec<_> = vec![
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    loop {
+        let mut str = String::new();
+        let bytes: usize = io::stdin().read_line(&mut str).expect("Cant read?!");
+
+        // Check for EoF.
+        if bytes == 0 {
+            break;
+        }
+
+        println!("{bytes}: {}", str.trim());
+
+        let mut first: i32 = -1;
+        let mut last: i32 = -1;
+        let mut index: usize = 0;
+
+        while index < str.len() {
+            // Check if this char is a string.
+            let c: char = str.as_bytes()[index] as char;
+            if char::is_digit(c, 10) {
+                if first < 0 {
+                    first = c.to_digit(10).unwrap() as i32;
+                    last = first;
+                } else {
+                    last = c.to_digit(10).unwrap() as i32
+                }
+            }
+
+            // Try to match this substring to a number string.
+            for (i, s) in strings.iter().enumerate() {
+                let e: usize = std::cmp::min(s.len(), str.len() - index);
+
+                if *s == &str[index..index + e] {
+                    println!("Found a numString");
+                    if first < 0 {
+                        first = i as i32;
+                        last = first;
+                    } else {
+                        last = i as i32;
+                    }
+                }
+            }
+
+            // Next item.
+            index += 1;
+        }
+
+        println!("For this line: {first}{last}");
+        sum += 10 * first + last;
+    }
+
+    println!("{sum}");
+}
