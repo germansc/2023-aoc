56ade32e18b67bcc180c6e88aa2285a4240991a2
germansc <germanscillone@gmail.com>
2023-12-04 10:02:08 -0300

    [day04] Removed unnecesary compiled files, and update .gititgnore to avoid future issues.

--- day04/target/debug/day04.d
+++
@@ -1,1 +0,0 @@
-/home/germansc/workspace/2023-aoc/day04/target/debug/day04: /home/germansc/workspace/2023-aoc/day04/src/main.rs
--- day04/target/debug/deps/day04-2f3552f427ca5818.d
+++
@@ -1,5 +0,0 @@
-/home/germansc/workspace/2023-aoc/day04/target/debug/deps/libday04-2f3552f427ca5818.rmeta: src/main.rs
-
-/home/germansc/workspace/2023-aoc/day04/target/debug/deps/day04-2f3552f427ca5818.d: src/main.rs
-
-src/main.rs:
