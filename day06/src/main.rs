dca683e81d79760bf400c8b667163cf845f011d2
germansc <germanscillone@gmail.com>
2023-12-06 08:37:48 -0300

    [day06] Added solution for first part of day 6.

---
+++ day06/src/main.rs
@@ -0,0 +1,85 @@
+use std::io;
+
+fn main() {
+    println!("2023 AoC - Day 06");
+    // --------------------------------------------------- INPUT COLLECTION ---
+    // Get Time from the first line
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("could not read stdin");
+
+    let time: Vec<u64> = line
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(" ")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    // Get Distance from the first line
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("could not read stdin");
+
+    let dist: Vec<u64> = line
+        .split(":")
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(" ")
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    // ------------------------------------------------------------- PART 1 ---
+    let _winners = get_winning_ranges(&time, &dist);
+    let mut part1: u64 = 1;
+    for range in _winners {
+        part1 *= (range.1 + 1) - range.0;
+    }
+
+    println!("PART 1: {part1}");
+}
+
+fn get_winning_ranges(time: &Vec<u64>, dist: &Vec<u64>) -> Vec<(u64, u64)> {
+    let mut results: Vec<(u64, u64)> = vec![];
+
+    // Find the first winning number and last winning numbers
+    for (i, target) in dist.iter().enumerate() {
+        let mut low = 0;
+        let mut hig = 0;
+
+        for held in 0..=time[i] {
+            if ((time[i] - held) * held) > *target {
+                low = held;
+                break;
+            }
+        }
+
+        for held in (0..time[i]).rev() {
+            if ((time[i] - held) * held) > *target {
+                hig = held;
+                break;
+            }
+        }
+
+        println!(
+            "Winning range = {},{} [... {} {} ... {} {} ...]/{}",
+            low,
+            hig,
+            ((time[i] - (low - 1)) * (low - 1)),
+            ((time[i] - low) * low),
+            ((time[i] - hig) * hig),
+            ((time[i] - (hig + 1)) * (hig + 1)),
+            *target
+        );
+
+        results.push((low, hig));
+    }
+
+    return results;
+}
