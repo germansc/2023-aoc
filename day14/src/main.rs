9080cfae4c446c2ade1a2ba79ecf9f04942cdf09
germansc <germanscillone@gmail.com>
2023-12-14 22:58:46 -0300

    [day14] Added solution for part 2.

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,6 +1,9 @@
-use std::io::{stdin, Read};
+use std::{
+    collections::HashMap,
+    io::{stdin, Read},
+};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Map {
     width: usize,
     height: usize,
@@ -8,7 +11,7 @@ struct Map {
 }
 
 impl Map {
-    fn print(&self) {
+    fn _print(&self) {
         println!("Map {}x{}", self.width, self.height);
         for i in 0..self.chars.len() {
             if i != 0 && i % self.width as usize == 0 {
@@ -30,7 +33,7 @@ impl Map {
         return Some(index);
     }
 
-    fn to_point(&self, idx: usize) -> Option<(usize, usize)> {
+    fn _to_point(&self, idx: usize) -> Option<(usize, usize)> {
         if idx >= self.chars.len() {
             return None;
         }
@@ -69,6 +72,22 @@ impl Map {
 
         return self;
     }
+
+    fn roll_clockwise(self) -> Map {
+        let mut new_buff: Vec<char> = vec![];
+
+        for x in 0..self.width {
+            let mut col = self.get_column(x);
+            col.reverse();
+            new_buff.append(&mut col);
+        }
+
+        return Map {
+            width: self.height,
+            height: self.width,
+            chars: new_buff,
+        };
+    }
 }
 
 fn main() {
@@ -103,13 +122,72 @@ fn main() {
 
     // map.print();
     for col in 0..map.width {
-        let rolled_col = roll_column(map.get_column(col));
+        let rolled_col = tilt_column(map.get_column(col));
         tilted_map = tilted_map.set_column(col, &rolled_col);
 
         part1 += score_column(&rolled_col);
     }
 
     println!("PART 1: {part1}");
+
+    // Part 2 .---------------------------------------------------------------
+    //
+    let mut cycle_cache: HashMap<Map, u64> = HashMap::new();
+    let mut scores: Vec<u64> = vec![];
+    let mut cycles = 0;
+    let period;
+    let offset;
+
+    // Perform cycles and store the results unitl a repeated condition is found.
+    let mut rollable_map = map.clone();
+    loop {
+        // Tilt North, West, South and East.
+        for _ in 0..4 {
+            // Tilt North, West, South and East.
+            for col in 0..rollable_map.width {
+                let tilted_column = tilt_column(rollable_map.get_column(col));
+                rollable_map = rollable_map.set_column(col, &tilted_column);
+            }
+
+            rollable_map = rollable_map.roll_clockwise();
+        }
+
+        cycles += 1;
+        let mut cycle_score = 0;
+        for col in 0..rollable_map.width {
+            cycle_score += score_column(&rollable_map.get_column(col));
+        }
+
+        // println!("Cycle {cycles}: {cycle_score}");
+        // rollable_map._print();
+        // println!("");
+
+        // Check if this condition was already seen, or store its load if new.
+        let key_map = rollable_map.clone();
+        match cycle_cache.get(&key_map) {
+            Some(val) => {
+                period = cycles - val;
+                offset = val;
+                println!(
+                    "Detected a Cycle! This board is the same as {}, so it repeats every {} rotations!",
+                    val,
+                    period
+                );
+
+                break;
+            }
+            None => {
+                cycle_cache.insert(key_map, cycles);
+                scores.push(cycle_score);
+            }
+        }
+    }
+
+    println!("Period: {period} | Offset: {offset}");
+
+    let idx = (1000000000 - offset) % period;
+    let idx = (idx + offset) as usize;
+    println!("PART 2: {idx} {}", scores[idx - 1]);
 }
 
 fn score_column(column: &Vec<char>) -> u64 {
@@ -124,7 +202,7 @@ fn score_column(column: &Vec<char>) -> u64 {
     return sum as u64;
 }
 
-fn roll_column(column: Vec<char>) -> Vec<char> {
+fn tilt_column(column: Vec<char>) -> Vec<char> {
     let mut empty_spot = 0;
     let mut new_col = column.clone();
 
