b1869509eaa3cb67d156b6a15e9771af7ae847b2
germansc <germanscillone@gmail.com>
2023-12-19 18:30:50 -0300

    [day19] Added a solution for part 2.

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -111,7 +111,7 @@ fn parse_input() -> (HashMap<String, Workflow>, Vec<Part>) {
     return (workflows, part_list);
 }
 
-fn process_part1(workflows: HashMap<String, Workflow>, parts: Vec<Part>) -> u64 {
+fn process_part1(workflows: &HashMap<String, Workflow>, parts: Vec<Part>) -> u64 {
     let mut approved: Vec<Part> = vec![];
     'next_part: for part in parts {
         // Initial point.
@@ -154,6 +154,182 @@ fn process_part1(workflows: HashMap<String, Workflow>, parts: Vec<Part>) -> u64 
         .sum::<u64>();
 }
 
+fn approved_permutations(
+    workflows: &HashMap<String, Workflow>,
+    wf_name: &str,
+    ranges: ((u64, u64), (u64, u64), (u64, u64), (u64, u64)),
+) -> u64 {
+    let mut leftover_ranges = ranges.clone();
+    let mut sum: u64 = 0;
+
+    let wf = workflows.get(wf_name).expect("Could not find workflow");
+
+    // Calculate the ranges that split into each workflow
+    for rule in &wf.ruleset {
+        let mut new_ranges = ((0, 0), (0, 0), (0, 0), (0, 0));
+        match rule.attribute {
+            0 => match rule.operation {
+                '>' => {
+                    new_ranges = (
+                        (rule.threshold + 1, leftover_ranges.0 .1),
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        (leftover_ranges.0 .0, rule.threshold),
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                }
+                '<' => {
+                    new_ranges = (
+                        (leftover_ranges.0 .0, rule.threshold - 1),
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        (rule.threshold, leftover_ranges.0 .1),
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                }
+                _ => {}
+            },
+            1 => match rule.operation {
+                '>' => {
+                    new_ranges = (
+                        leftover_ranges.0,
+                        (rule.threshold + 1, leftover_ranges.1 .1),
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        (leftover_ranges.1 .0, rule.threshold),
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                }
+                '<' => {
+                    new_ranges = (
+                        leftover_ranges.0,
+                        (leftover_ranges.1 .0, rule.threshold - 1),
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        (rule.threshold, leftover_ranges.1 .1),
+                        leftover_ranges.2,
+                        leftover_ranges.3,
+                    );
+                }
+                _ => {}
+            },
+            2 => match rule.operation {
+                '>' => {
+                    new_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        (rule.threshold + 1, leftover_ranges.2 .1),
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        (leftover_ranges.2 .0, rule.threshold),
+                        leftover_ranges.3,
+                    );
+                }
+                '<' => {
+                    new_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        (leftover_ranges.2 .0, rule.threshold - 1),
+                        leftover_ranges.3,
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        (rule.threshold, leftover_ranges.2 .1),
+                        leftover_ranges.3,
+                    );
+                }
+                _ => {}
+            },
+            3 => match rule.operation {
+                '>' => {
+                    new_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        (rule.threshold + 1, leftover_ranges.3 .1),
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        (leftover_ranges.3 .0, rule.threshold),
+                    );
+                }
+                '<' => {
+                    new_ranges = (
+                        leftover_ranges.0.clone(),
+                        leftover_ranges.1.clone(),
+                        leftover_ranges.2.clone(),
+                        (leftover_ranges.3 .0, rule.threshold - 1),
+                    );
+                    leftover_ranges = (
+                        leftover_ranges.0,
+                        leftover_ranges.1,
+                        leftover_ranges.2,
+                        (rule.threshold, leftover_ranges.3 .1),
+                    );
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+
+        // dbg!(&wf);
+        // dbg!(&new_ranges);
+        // println!("-> continue to {}", rule.target);
+        match rule.target.as_str() {
+            "A" => sum += ranges_to_permutations(new_ranges),
+            "R" => {}
+            val => sum += approved_permutations(workflows, val, new_ranges),
+        }
+    }
+
+    // dbg!(&leftover_ranges);
+    // println!("-> continue to {}", wf.default);
+    match wf.default.as_str() {
+        "A" => sum += ranges_to_permutations(leftover_ranges),
+        "R" => {}
+        val => sum += approved_permutations(workflows, val, leftover_ranges),
+    }
+
+    return sum;
+}
+
+fn ranges_to_permutations(ranges: ((u64, u64), (u64, u64), (u64, u64), (u64, u64))) -> u64 {
+    return (1 + ranges.0 .1 - ranges.0 .0)
+        * (1 + ranges.1 .1 - ranges.1 .0)
+        * (1 + ranges.2 .1 - ranges.2 .0)
+        * (1 + ranges.3 .1 - ranges.3 .0);
+}
+
+fn process_part2(workflows: &HashMap<String, Workflow>) -> u64 {
+    // All possible parts have attributes in the range 1 to 4000.
+    let initial_ranges = ((1, 4000), (1, 4000), (1, 4000), (1, 4000));
+
+    return approved_permutations(workflows, "in", initial_ranges);
+}
+
 fn main() {
     println!("2023 AoC - Day 19");
 
@@ -162,7 +338,13 @@ fn main() {
 
     // Part 1 ---------------------------------------------------------------
 
-    let part1 = process_part1(workflows, parts);
+    let part1 = process_part1(&workflows, parts);
 
     println!("PART 1: {part1}");
+
+    // Part 2 ---------------------------------------------------------------
+
+    let part2 = process_part2(&workflows);
+
+    println!("PART 2: {part2}");
 }
