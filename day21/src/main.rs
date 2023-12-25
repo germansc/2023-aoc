29be32e60e2304c4397344033c3f0b9a2edc7d8e
germansc <germanscillone@gmail.com>
2023-12-25 18:47:32 -0300

    [day21] WIP on part2. Couldn't make it.

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -46,6 +46,18 @@ impl Map {
         return Some(index as usize);
     }
 
+    fn can_walk_part2(&self, x: i64, y: i64) -> Option<usize> {
+        let x = (self.width as i64 + x) % self.width as i64;
+        let y = (self.height as i64 + y) % self.height as i64;
+
+        let index = y * self.width as i64 + x;
+        if self.chars[index as usize] == '#' {
+            return None;
+        }
+
+        return Some(index as usize);
+    }
+
     fn print(&self) {
         println!("Map {}x{}", self.width, self.height);
         for i in 0..self.height {
@@ -113,10 +125,57 @@ fn process_part1(map: &Map) -> u64 {
 }
 
 fn process_part2(map: &Map) -> u64 {
-    // Let's see...
+    // Walk the cells figuring out which are acessible at each iteration.
+    let mut nodes: Vec<usize> = vec![];
+    let mut record: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut steps = 0;
+
+    // Add the initial spot.
+    nodes.push(
+        map.chars
+            .iter()
+            .position(|c| *c == 'S')
+            .expect("Start not found?"),
+    );
+
+    while steps < 26501365 {
+        let mut next_nodes: Vec<usize> = vec![];
+        while nodes.len() != 0 {
+            let idx = nodes.pop().unwrap();
+
+            // Check if this cell was already evaulated.
+            match record.get(&idx) {
+                Some(cells) => next_nodes.extend(cells),
+                None => {
+                    let (x, y) = map.to_point(idx).unwrap();
+                    let mut cell_nodes: Vec<usize> = vec![];
+                    for dir in [(1, 0), (-1, 0), (0, 1), (0, -1)] {
+                        if let Some(index) = map.can_walk_part2(x + dir.0, y + dir.1) {
+                            cell_nodes.push(index);
+                        }
+                    }
+
+                    record.insert(idx, cell_nodes.clone());
+                    next_nodes.extend(cell_nodes);
+                }
+            }
+        }
+
+        // Remove duplicates.
+        next_nodes.sort();
+        next_nodes.dedup();
+
+        steps += 1;
+        println!(
+            "After {steps} steps, the elf can reach {} cells.",
+            next_nodes.len()
+        );
+
+        nodes = next_nodes;
+    }
 
     // Return the value.
-    return 0;
+    return nodes.len() as u64;
 }
 
 fn main() {
